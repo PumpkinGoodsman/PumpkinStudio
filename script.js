4add8d79d39e1e996d94c919264a0a689685061a
PumpkinGoodsman <134625042+PumpkinGoodsman@users.noreply.github.com>
2023-05-25 08:06:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var header = document.getElementById('myHeader');
+
+  header.addEventListener('click', function() {
+    header.style.backgroundColor = 'red';
+  });
+});
